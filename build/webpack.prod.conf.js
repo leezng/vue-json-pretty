1b5d8160dab86fd0808fc8e1d0f9717b68fee96f
Rafał Chłodnicki <rchl2k@gmail.com>
2020-09-12 12:50:27 +0200

    Bundle CSS file separately

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -59,6 +59,17 @@ if (isDist) {
     libraryTarget: 'umd'
   }
   webpackConfig.plugins.push(
+    // extract css into its own file
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    // Compress extracted CSS. We are using this plugin so that possible
+    // duplicated CSS from different components can be deduped.
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: {
+        safe: true
+      }
+    }),
     new CopyWebpackPlugin({
       patterns: [
         {
